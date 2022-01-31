86a3c1132f9e3c4b59ba16d722d1e49e1e537638
Ching-lin LEE <chilee@student.42.fr>
2022-01-31 18:49:54 +0100

    So_long Final

--- sources/srcs/matrix_creation.c
+++ sources/srcs/matrix_creation.c
@@ -6,11 +6,38 @@
 /*   By: chilee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:44:21 by chilee            #+#    #+#             */
-/*   Updated: 2022/01/27 12:12:42 by chilee           ###   ########.fr       */
+/*   Updated: 2022/01/31 13:43:51 by chilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
+
+int	double_newline(char *s)
+{
+	while (*s)
+	{
+		if (*s == '\n' && *(s + 1))
+		{
+			s++;
+			if (*s != '\n')
+				s++;
+			else
+				return (1);
+		}
+		if (*s != '\0')
+			s++;
+	}
+	return (0);
+}
+
+char	**my_exit(int fd, char *str)
+{
+	close(fd);
+	free(str);
+	ft_putstr(KRED"Error\n"KNRM);
+	ft_putstr(KYEL"Only 1 newline allowed before the end of map"KNRM);
+	exit (0);
+}
 
 char	**create_matrix(char *map_av)
 {
@@ -33,6 +60,8 @@ char	**create_matrix(char *map_av)
 		free(buff);
 		buff = get_next_line(fd);
 	}
+	if (double_newline(strmap))
+		return (my_exit(fd, strmap));
 	created_matrix = ft_split(strmap, '\n');
 	free(strmap);
 	close(fd);
--- sources/srcs_bonus/matrix_creation_bonus.c
+++ sources/srcs_bonus/matrix_creation_bonus.c
@@ -1,16 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   matrix_creation.c                                  :+:      :+:    :+:   */
+/*   matrix_creation_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chilee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 11:22:07 by chilee            #+#    #+#             */
-/*   Updated: 2022/01/26 11:36:24 by chilee           ###   ########.fr       */
+/*   Updated: 2022/01/31 12:18:36 by chilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long_bonus.h"
+
+int	double_newline(char *s)
+{
+	while (*s)
+	{
+		if (*s == '\n' && *(s + 1))
+		{
+			s++;
+			if (*s != '\n')
+				s++;
+			else
+				return (1);
+		}
+		if (*s != '\0')
+			s++;
+	}
+	return (0);
+}
+
+char	**my_exit(int fd, char *str)
+{
+	close(fd);
+	free(str);
+	ft_putstr(KGRN"Error\nOnly 1 newline allowed before the end of map"KNRM);
+	exit (0);
+}
 
 char	**create_matrix(char *map_av)
 {
@@ -33,6 +59,8 @@ char	**create_matrix(char *map_av)
 		free (buff);
 		buff = get_next_line(fd);
 	}
+	if (double_newline(strmap))
+		return (my_exit(fd, strmap));
 	created_matrix = ft_split(strmap, '\n');
 	free (strmap);
 	return (created_matrix);
